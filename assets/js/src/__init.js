e7e7c335710ff5831b247ff5a257a32dcc681fa2
Kelvin Nguyen <kelvinnuci@gmail.com>
2016-02-28 19:55:34 -0800

    Bump up version to next major version

--- assets/js/src/__init.js
+++ assets/js/src/__init.js
@@ -3,7 +3,7 @@
 $(function() {
     var CaffeineTheme;
     return window.CaffeineTheme = CaffeineTheme = {
-        version: '1.3.0',
+        version: '2.0.0',
         search: {
             container: function() {
                 return $('#results');
