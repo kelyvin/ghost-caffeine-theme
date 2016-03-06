36f71d0d731bc8db17600afea716af3b98ce4b55
Kelvin Nguyen <kelvinnuci@gmail.com>
2016-03-05 18:06:58 -0800

    Minor verison bump with latest optimization changes

--- assets/js/src/__init.js
+++ assets/js/src/__init.js
@@ -4,7 +4,7 @@ $(function() {
     var CaffeineTheme;
 
     window.CaffeineTheme = CaffeineTheme = {
-        version: "2.1.0",
+        version: "2.2.0",
         search: {
             container: function() {
                 return $("#results");
