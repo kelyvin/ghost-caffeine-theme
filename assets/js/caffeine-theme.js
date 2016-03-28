b8623df09a989222eca7b36cff3adc30e26f1791
Kelvin Nguyen <kelvinnuci@gmail.com>
2016-03-28 02:42:38 -0700

    Fix pagination bug when navigation back to home page

--- assets/js/caffeine-theme.js
+++ assets/js/caffeine-theme.js
@@ -1,6 +1,6 @@
 /**
  * caffeine-theme - Minimalist and Elegant theme for Ghost
- * @version 2.4.2
+ * @version 2.5.0
  * @link    https://github.com/kelyvin/caffeine-theme
  * @author  Kelyvin (https://github.com/kelyvin)
  * @license MIT
