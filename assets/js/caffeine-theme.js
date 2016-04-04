4731ddbd9766bab0b8199f8c0321e824f6d775eb
Kelvin Nguyen <kelvinnuci@gmail.com>
2016-04-03 17:02:09 -0700

    Version bump

--- assets/js/caffeine-theme.js
+++ assets/js/caffeine-theme.js
@@ -1,6 +1,6 @@
 /**
  * caffeine-theme - Minimalist and Elegant theme for Ghost
- * @version 2.5.1
+ * @version 2.5.2
  * @link    https://github.com/kelyvin/caffeine-theme
  * @author  Kelyvin (https://github.com/kelyvin)
  * @license MIT
