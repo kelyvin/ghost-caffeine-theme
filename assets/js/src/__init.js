fecff5624665cc1f6add8f05b52d57f9eb676638
Kelvin Nguyen <kelvinnuci@gmail.com>
2016-05-16 16:21:06 -0700

    Fix styling for links for both images and text

--- assets/js/src/__init.js
+++ assets/js/src/__init.js
@@ -5,7 +5,7 @@ $(function() {
         openHash = "#open";
 
     window.CaffeineTheme = CaffeineTheme = {
-        version: "2.7.1",
+        version: "2.7.2",
         search: {
             container: function() {
                 return $("#results");
