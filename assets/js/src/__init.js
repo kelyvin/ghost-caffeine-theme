71eebe6a99478c5adb2472424b857d2052644fc5
Kelvin Nguyen <kelvinnuci@gmail.com>
2017-08-13 02:31:19 -0700

    Upgrade to v3.0 to support ghost v1

--- assets/js/src/__init.js
+++ assets/js/src/__init.js
@@ -5,7 +5,7 @@ $(function() {
         openHash = "#open";
 
     window.CaffeineTheme = CaffeineTheme = {
-        version: "2.9.0",
+        version: "3.0.0",
         search: {
             container: function() {
                 return $("#results");
