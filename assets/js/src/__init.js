8d8436a15b6ddf019ad2ea6d02f8f59e82549358
Kelvin Nguyen <kelvinnuci@gmail.com>
2017-10-29 18:00:30 -0700

    Add config to package.json to specify number of posts per page

--- assets/js/src/__init.js
+++ assets/js/src/__init.js
@@ -5,7 +5,7 @@ $(function() {
         openHash = "#open";
 
     window.CaffeineTheme = CaffeineTheme = {
-        version: "3.0.1",
+        version: "3.0.2",
         search: {
             container: function() {
                 return $("#results");
