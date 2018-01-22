32a10bec3b265a3f564fc2490aff5b59a1cffa31
Kelvin Nguyen <kelvinnuci@gmail.com>
2018-01-21 22:00:33 -0800

    Fix youtube embed layout issues

--- assets/js/src/__init.js
+++ assets/js/src/__init.js
@@ -5,7 +5,7 @@ $(function() {
         openHash = "#open";
 
     window.CaffeineTheme = CaffeineTheme = {
-        version: "3.0.3",
+        version: "3.0.4",
         search: {
             container: function() {
                 return $("#results");
